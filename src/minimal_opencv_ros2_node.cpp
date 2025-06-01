0ecbd9fee0f0aaf724aa36b6a5f3db3c689596bd
om-mahajan <49646667+om-mahajan@users.noreply.github.com>
2025-06-02 01:57:19 +0530

    Update minimal_opencv_ros2_node.cpp

--- src/minimal_opencv_ros2_node.cpp
+++ src/minimal_opencv_ros2_node.cpp
@@ -19,29 +19,25 @@ public:
 
 private:
   void timer_callback() {
-  // Load your image from file (replace with your actual path)
-  std::string image_path = "/home/om/ardu_ws/src/opencv/src/p1.jpeg";
+  std::string image_path = "/home/om/ardu_ws/src/opencv/src/p1.jpeg"; // change image path
   cv::Mat my_image = cv::imread(image_path);
 
-  // Convert to HSV
   cv::Mat hsv_image;
   cv::cvtColor(my_image, hsv_image, cv::COLOR_BGR2HSV);
 
-  // Yellow range in HSV
+  // Yellow
   cv::Scalar lower_yellow(20, 100, 100);
   cv::Scalar upper_yellow(30, 255, 255);
   cv::Mat yellow_mask;
   cv::inRange(hsv_image, lower_yellow, upper_yellow, yellow_mask);
 
-  // Find contours
   std::vector<std::vector<cv::Point>> contours;
   cv::findContours(yellow_mask, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
-
-  // Draw bounding boxes around yellow regions
+ // replace with geotagging section
   for (const auto& contour : contours) {
     if (cv::contourArea(contour) < 100) continue; // Ignore noise
     cv::Rect box = cv::boundingRect(contour);
-    cv::rectangle(my_image, box, cv::Scalar(255, 0, 0), 2); // Blue box
+    cv::rectangle(my_image, box, cv::Scalar(255, 0, 0), 2); 
   }
 
   // Convert and publish
@@ -61,9 +57,8 @@ private:
 
 int main(int argc, char *argv[]) {
   rclcpp::init(argc, argv);
-  // create a ros2 node
+  
   auto node = std::make_shared<MinimalImagePublisher>();
-  // process ros2 callbacks until receiving a SIGINT (ctrl-c)
   rclcpp::spin(node);
   rclcpp::shutdown();
   return 0;
